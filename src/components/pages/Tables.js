16dbe709184f491499d9f2c1f690f91beb864a8f
adanbae <mostbae@naver.com>
2019-06-29 22:46:56 +0900

    테이블 페이지 추가

---
+++ src/components/pages/Tables.js
@@ -0,0 +1,93 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+// Material-ui 컴포넌트들
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 700
+  }
+});
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
+];
+
+const Tables = props => {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <Typography variant="h3" noWrap>
+        테이블 (Tables)
+      </Typography>
+      <br />
+      <Typography paragraph>
+        <a
+          href="https://material.io/tools/icons/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          머터리얼(Material) 아이콘
+        </a>
+        을 사용합니다. 이 관리자 페이지는는 Material-UI frameworks를 채택했기
+        때문이죠.
+        <br />
+        (추후 다른 아이콘도 지원을 생각합니다.)
+      </Typography>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat (g)</TableCell>
+              <TableCell align="right">Carbs (g)</TableCell>
+              <TableCell align="right">Protein (g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </React.Fragment>
+  );
+};
+
+Tables.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Tables);
