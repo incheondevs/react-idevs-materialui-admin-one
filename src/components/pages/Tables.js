e2c3eabcbe9a3b7d898898d4a9771f343cbc9396
adanbae <mostbae@naver.com>
2019-07-03 23:04:50 +0900

    HOC 배제 테이블 소스 수정(Materialui v4.0)

--- src/components/pages/Tables.js
+++ src/components/pages/Tables.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
+//import PropTypes from "prop-types";
+import { makeStyles } from '@material-ui/core/styles';
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -10,76 +10,77 @@ import Paper from "@material-ui/core/Paper";
 // Material-ui 컴포넌트들
 import Typography from "@material-ui/core/Typography";
 
-const styles = theme => ({
+
+
+const useStyles = makeStyles(theme => ({
   root: {
-    width: "100%",
-    marginTop: theme.spacing.unit * 3,
-    overflowX: "auto"
+    width: '100%',
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    width: '100%',
+    overflowX: 'auto',
+    marginBottom: theme.spacing(2),
   },
   table: {
-    minWidth: 700
-  }
-});
+    minWidth: 650,
+  },
+}));
 
-let id = 0;
 function createData(name, calories, fat, carbs, protein) {
-  id += 1;
-  return { id, name, calories, fat, carbs, protein };
+  return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9)
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-const Tables = props => {
-  const { classes } = props;
+export default function DenseTable() {
+  const classes = useStyles();
+
   return (
-    <React.Fragment>
-      <Typography variant="h3" noWrap>
-        다목적 테이블 리스트
-      </Typography>
-      <br />
-      <Typography paragraph>
-        원하는 목적에 맞는 테이블 스타일을 도입해 사용해 보세요.
+    <div className={classes.root}>
+      <React.Fragment>
+        <Typography variant="h3" noWrap>
+          다목적 테이블 리스트
+        </Typography>
         <br />
-        더욱 직관적이고 편리하게 확인할 수 있습니다.
-      </Typography>
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat (g)</TableCell>
-              <TableCell align="right">Carbs (g)</TableCell>
-              <TableCell align="right">Protein (g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map(row => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+        <Typography paragraph>
+          원하는 목적에 맞는 테이블 스타일을 도입해 사용해 보세요.
+          <br />
+          더욱 직관적이고 편리하게 확인할 수 있습니다.
+        </Typography>
+        <Paper className={classes.paper}>
+          <Table className={classes.table} size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
-    </React.Fragment>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </React.Fragment>
+    </div>
   );
-};
-
-Tables.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Tables);
+}
