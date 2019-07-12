8ed467d5013a85d429adf10210ae93bc07c2a1c2
SBJang <denlyou85@gmail.com>
2019-07-12 12:35:49 +0900

    상대 경로 처리용 코드 추가

--- src/history.js
+++ src/history.js
@@ -1,2 +1,5 @@
 import { createBrowserHistory } from 'history';
 export default createBrowserHistory();
+/** 해시라우터를 사용하려면 위를 주석처리하고 아래 주석해제 **/
+// import { createHashHistory } from 'history';
+// export default createHashHistory();
